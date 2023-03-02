a41dcdc00aabcc0abf4ef784b563f1dc8b3a54cb
Zubayer Mohammad <zubayer@gmail.com>
2023-03-02 07:18:05 +0000

    Add user data files

---
+++ iac/install_kubernetes.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.25.6/2023-01-30/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
